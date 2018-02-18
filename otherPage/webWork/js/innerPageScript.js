c07ac03a3380d1326a9e829307d441b1ffb3af04
Mimick <mimick.zjl@outlook.com>
2018-02-18 23:06:17 +0800

    fixInnerJSbug
    Co-Authored-By: Mimick <mimick.zjl@gmail.com>

--- otherPage/webWork/js/innerPageScript.js
+++ otherPage/webWork/js/innerPageScript.js
@@ -47,7 +47,14 @@ window.onload = function () {
 			return function () {
 				n_bannerli_now = ii;
 				console.log(ii);
-				window.location.href = window.location.href + '#' + div_content_bindBannerElements[ii].id;
+				//获取当前文档名
+				var currentDocName = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+				var jingIndex = currentDocName.indexOf('#');
+				if (jingIndex != -1) {
+					currentDocName = currentDocName.substring(0, jingIndex);
+				}
+				console.log('currentDocName:' + currentDocName);
+				window.location.href = currentDocName + '#' + div_content_bindBannerElements[ii].id;
 				//如果被banner覆蓋則向下滑動60空出banner位置
 				if (div_content_bindBannerElements[ii].getBoundingClientRect().top < 59) {
 					//pageXOffset與pageYOffset為scrollX與scrollY的別名，為了兼容IE用前兩個
